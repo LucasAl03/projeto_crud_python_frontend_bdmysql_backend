25caa587784acb7f7ccc1ac631d9612dde304c0b
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-01 21:26:46 -0300

    Copia

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.pessoa_route import router as pessoa_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods = ["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(pessoa_router)
+
+"""
+    PARA EXECUTAR O PROJETO DIGITE A LINHA DE COMANDO:
+    uvicorn main:app --reload
+
+    python -m  uvicorn main:app --reload
+"""
