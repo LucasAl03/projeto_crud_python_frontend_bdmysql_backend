6fa6a937a7dd1ebda0b70f06a0af477fc117df2b
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-09-04 21:19:59 -0300

    Correções em distancia

--- models/corrida_model.py
+++ models/corrida_model.py
@@ -8,8 +8,8 @@ class Corrida(Base):
     idcorrida = Column(Integer, primary_key=True, index=True)
     descricao_corrida = Column(String(200))
     data_corrida = Column(Date)
-    distancia_5km = Column(String(5))
-    distancia_10km = Column(String(5))
-    distancia_25km = Column(String(5))
+    distancia_5km = Column(Boolean(1))
+    distancia_10km = Column(Boolean(1))
+    distancia_25km = Column(Boolean(1))
     
     
--- schemas/corrida_schema.py
+++ schemas/corrida_schema.py
@@ -4,9 +4,9 @@ from datetime import date
 class CorridaSchema(BaseModel):
     descricao_corrida : str
     data_corrida: date
-    distancia_5km: str
-    distancia_10km: str
-    distancia_25km: str
+    distancia_5km: bool
+    distancia_10km: bool
+    distancia_25km: bool
 
 
     
